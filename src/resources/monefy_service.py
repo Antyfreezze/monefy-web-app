f66b18387ffa11eca489b86b7954ee9769660b6a
Bohdan Solianyk <bohdan_solianyk@epam.com>
2022-01-23 00:15:28 +0200

    create DropboxClient class, add blueprints, CBV, logging, update endpoints names, add function to provide run configuration
    create DropboxClient class, add blueprints, CBV, logging, update endpoints names, add function to provide run configuration

---
+++ src/resources/monefy_service.py
@@ -0,0 +1,65 @@
+import os
+import hmac
+from hashlib import sha256
+from http import HTTPStatus
+
+from sanic.log import logger
+from sanic.views import HTTPMethodView
+from sanic.response import json, text, HTTPResponse
+from sanic.exceptions import Forbidden, NotFound
+
+from src.domain.dropbox_utils import DropboxClient
+
+
+class ViewWithDropboxClient(HTTPMethodView):
+
+    def __init__(self):
+        super().__init__()
+        self.dropbox_client = DropboxClient()
+        self.secret = os.environ.get("DROPBOX_APP_SECRET")
+
+
+class HealthCheck(HTTPMethodView):
+
+    async def get(self, request) -> HTTPResponse:
+        """Function for smoke test"""
+        return json({"message": "Hello world!"})
+
+
+class MonefyInfo(ViewWithDropboxClient):
+
+    async def get(self, request) -> HTTPResponse:
+        """Returns JSON formatted monefy transactions from csv files"""
+        monefy_stats = self.dropbox_client.get_monefy_info()
+        if not monefy_stats:
+            raise NotFound("Monefy csv files not found")
+        return json(monefy_stats)
+
+    async def post(self, request) -> HTTPResponse:
+        """Write existing monefy csv files with transaction to instance"""
+        write_result = self.dropbox_client.write_monefy_info()
+        return json({"message": write_result}, status=HTTPStatus.OK)
+
+
+class DropboxWebhook(ViewWithDropboxClient):
+
+    async def get(self, request) -> HTTPResponse:
+        """Respond to the webhook verification (GET request) by echoing back the challenge parameter"""
+        logger.info("verify dropbox webhook")
+        resp = request.args.get("challenge")
+        return text(
+            resp,
+            headers={"Content-Type": "text/plain", "X-Content-Type-Options": "nosniff"},
+            status=HTTPStatus.OK
+        )
+
+    async def post(self, request) -> HTTPResponse:
+        """Write csv files stored in Dropbox storage to instance"""
+        logger.info(f"write files by dropbox webhook {request.body}")
+        # Make sure this is a valid request from Dropbox
+        signature = request.headers.get("X-Dropbox-Signature", "InvalidSignature")
+        if not hmac.compare_digest(signature, hmac.new(self.secret.encode(), request.body, sha256).hexdigest()):
+            logger.error(f"Dropbox webhook validation check failed: Request forbidden")
+            raise Forbidden("Request forbidden", status_code=HTTPStatus.FORBIDDEN)
+        result = self.dropbox_client.write_monefy_info()
+        return json({"message": result}, status=HTTPStatus.OK)
